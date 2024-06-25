14292383f58732db2fcab706e11dc0174e72c622
harendra <harendra@users.noreply.github.com>
2024-06-25 16:44:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
 
